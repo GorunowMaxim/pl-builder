057a853eca9e1ca5c09a518ecd770f39e9228cb2
Gorunow Maxim <mgorunow@gmail.com>
2024-10-29 00:25:58 +0400

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pl-builder'
+  base: '/pl-builder',
+  resolve: {
+    alias: {
+      widgets: '/src/widgets/*',
+      features: '/src/features/*',
+      enteties: '/src/enteties/*',
+      shared: '/src/shared/*',
+    }
+  }
 })
